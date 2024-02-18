792e8312f2dec584e7182055b7997cbd8ed4342c
mohamadKattan <kattansoftware@gmail.com>
2024-02-18 17:53:43 +0300

    17-2-24

--- src/db_config/db_config.js
+++ src/db_config/db_config.js
@@ -1,21 +1,6 @@
 import mysql from 'mysql';
 
-const options = {
-    host: 'localhost',
-    user: 'root',
-    password: 'mohamad88=',
-    database: 'shadow',
-    multipleStatements: true
 
-}
-
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mohamad88=",
-    database: "shadow"
-});
 
 var pool = mysql.createPool(options);
 const connectionDB = { con, pool,options }
